2c89060eb09514528140cbecf4a3bf6d04bf190a
Dibya Ghosh <dibya.ghosh@berkeley.edu>
2018-04-18 22:31:29 -0700

    It works now

--- gym_vision/encoders/deep_spatial_autoencoder.py
+++ gym_vision/encoders/deep_spatial_autoencoder.py
@@ -4,61 +4,71 @@ from torch.autograd import Variable
 import torch.nn as nn
 import torch.nn.functional as F
 
-class DeepSpatialAutoencoder(nn.Module):
-    """input_dim = 224, feature_point_dim = 16, reconstruct_dim=60"""
-    def __init__(input_dim, feature_point_dim, reconstruct_dim): 
-        super(DeepSpatialAutoencoder, self).__init__()
+class SpatialConvNetwork(nn.Module):
+    def __init__(self,input_dim, feature_point_dim):
+        super(SpatialConvNetwork,self).__init__()
         self.input_dim = input_dim
-        self.feature_point_dim = feature_point_dim # each one will have 2 coordinates representing x,y
-        self.reconstruct_dim = reconstruct_dim
+        self.feature_point_dim = feature_point_dim
+        self.conv1 = nn.Conv2d(3, 64, 7, stride=2)
+        self.conv2 = nn.Conv2d(64, 32, 5)
+        self.conv3 = nn.Conv2d(32, self.feature_point_dim, 5)
+        presoftmax_dim = int((input_dim - 6) / 2 - 8) #  hardcoded based on dimensionality of conv layers
+        self.x_mask = Variable(torch.from_numpy(np.array([i * np.ones(presoftmax_dim) / presoftmax_dim for i in range(presoftmax_dim)]).flatten().astype(np.float32)).cuda())
+        self.y_mask = Variable(torch.from_numpy(np.array([np.arange(presoftmax_dim) / presoftmax_dim for i in range(presoftmax_dim)]).flatten().astype(np.float32)).cuda())
+        print(self.x_mask.shape)
 
-        self.alpha = torch.tensor([1], requires_grad=True) # scalar value?
-
-        self.build_conv_layers()
-
-        presoftmax_dim = (input_dim - 6) / 2 - 12 #  hardcoded based on dimensionality of conv layers
-        x_mask = np.flatten(np.array([i * np.ones(presoftmax_dim) for i in range(presoftmax_dim)]))
-        y_mask = np.flatten(np.array([np.arange(presoftmax_dim) for i in range(presoftmax_dim)])
-
-    def build_conv_layers(self):
-        self.conv1 = nn.Conv2D(3, 64, 7, stride=2)
-        self.conv2 = nn.Conv2D(64, 32, 5)
-        self.conv3 = nn.Conv2D(32, self.feature_point_dim, 5)
 
     def spatial_softmax(self, x):
         x_flat = x.view(x.shape[0], x.shape[1], -1) # N x channels x (presoftmax_dim)**2
-        softmaxed_x = F.softmax(x_flat / self.alpha, dim=2)
-        weighted_x = softmaxed_x * x_mask
-        weighted_y = softmaxed_x * y_mask
+        softmaxed_x = F.softmax(x_flat / .001, dim=2)
+        
+        #softmaxed_x = F.softmax(x_flat / self.alpha, dim=2)
+        weighted_x = softmaxed_x * self.x_mask
+        weighted_y = softmaxed_x * self.y_mask
 
-        mean_x = torch.mean(weighted_x, dim=2) # N x channels
-        mean_y = torch.mean(weighted_y, dim=2)
-
+        mean_x = torch.sum(weighted_x, dim=2) # N x channels
+        mean_y = torch.sum(weighted_y, dim=2)
+        #import ipdb; ipdb.set_trace()
         return torch.cat([mean_x, mean_y], dim=1) # N x (2 channels)
-
-    def reconstruct(self, feature_points):
-        output_dim = self.reconstruct_dim ** 2
-        input_dim = self.feature_point_dim * 2
-        reconstruction_net = nn.Linear(input_dim, output_dim)
-        reconstructed_output = reconstruction_net(feature_points)
-        return reconstructed_output.view(feature_points.shape[0], output_dim, output_dim)
 
     def forward(self, x):
         x = F.relu(self.conv1(x))
         x = F.relu(self.conv2(x))
         x = F.relu(self.conv3(x))
+        feature_points = self.spatial_softmax(x)
+        return feature_points
 
-        feature_points = spatial_softmax(x)
-        return reconstruct(feature_points)
-"""
-    Initialize like this
-    dsa = DeepSpatialAutoencoder(224, 16, 56)
-"""
-
-def train_step(x, y, dsa, optimizer):
-    optimizer.zero_grad()
-    loss = nn.MSELoss(dsa(x), x)
-    loss.backward()
-    optimizer.step()
+class DeepSpatialAutoencoder(nn.Module):
+    """input_dim = 224, feature_point_dim = 16, reconstruct_dim=60"""
+    def __init__(self, input_dim, feature_point_dim, reconstruct_dim): 
+        super(DeepSpatialAutoencoder, self).__init__()
 
 
+        self.feature_point_dim = feature_point_dim
+        self.reconstruct_dim = reconstruct_dim
+        output_dim = self.reconstruct_dim ** 2
+
+        self.feature_point_network = SpatialConvNetwork(input_dim,feature_point_dim)
+
+        self.reconstruction_net = nn.Sequential(
+            nn.Linear(feature_point_dim * 2, feature_point_dim * 10),
+            nn.ReLU(),
+            nn.Linear(feature_point_dim * 10 , feature_point_dim * 100),
+            nn.ReLU(),
+            nn.Linear(feature_point_dim * 100, output_dim),
+        )
+        
+    def forward(self, x):
+        feature_points = self.feature_point_network(x)
+        reconstructed_output = self.reconstruction_net(feature_points)
+        return reconstructed_output.view(feature_points.shape[0],self.reconstruct_dim,self.reconstruct_dim)
+
+
+def loss(model,image,ds_image,next_image):
+    criterion = nn.MSELoss()
+    pred_ds = model(image)
+    
+    #fp1 = model.feature_point_network(image)
+    #fp2 = model.feature_point_network(next_image)
+
+    return criterion(pred_ds,ds_image)# + 0.1 * criterion(fp1,fp2)
