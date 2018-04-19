8ae6e8f822dcaa4553122a881377a2770cf1b1ce
Aurick Zhou <azhou42@berkeley.edu>
2018-04-18 22:43:48 -0700

    implemented trainable alpha

--- gym_vision/encoders/deep_spatial_autoencoder.py
+++ gym_vision/encoders/deep_spatial_autoencoder.py
@@ -9,18 +9,21 @@ class SpatialConvNetwork(nn.Module):
         super(SpatialConvNetwork,self).__init__()
         self.input_dim = input_dim
         self.feature_point_dim = feature_point_dim
+        
+        self.log_alpha = nn.Parameter(torch.from_numpy(np.array([0])))
+        self.alpha = torch.exp(self.log_alpha)
+
+
         self.conv1 = nn.Conv2d(3, 64, 7, stride=2)
         self.conv2 = nn.Conv2d(64, 32, 5)
         self.conv3 = nn.Conv2d(32, self.feature_point_dim, 5)
         presoftmax_dim = int((input_dim - 6) / 2 - 8) #  hardcoded based on dimensionality of conv layers
         self.x_mask = Variable(torch.from_numpy(np.array([i * np.ones(presoftmax_dim) / presoftmax_dim for i in range(presoftmax_dim)]).flatten().astype(np.float32)).cuda())
         self.y_mask = Variable(torch.from_numpy(np.array([np.arange(presoftmax_dim) / presoftmax_dim for i in range(presoftmax_dim)]).flatten().astype(np.float32)).cuda())
-        print(self.x_mask.shape)
-
 
     def spatial_softmax(self, x):
         x_flat = x.view(x.shape[0], x.shape[1], -1) # N x channels x (presoftmax_dim)**2
-        softmaxed_x = F.softmax(x_flat / .001, dim=2)
+        softmaxed_x = F.softmax(x_flat / self.alpha, dim=2)
         
         #softmaxed_x = F.softmax(x_flat / self.alpha, dim=2)
         weighted_x = softmaxed_x * self.x_mask
